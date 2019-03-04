c4627e656a539d1ceb8b173677359b2319004dc5
Mikayla Clemons <mikaylaclemons@Mikaylas-MacBook-Pro.local>
2019-03-04 11:07:20 -0500

    Create structure for the ProductRepository.

--- src/main/java/com/detroitlabs/rainforest/data/ProductRepository.java
+++ src/main/java/com/detroitlabs/rainforest/data/ProductRepository.java
@@ -1,4 +1,24 @@
 package com.detroitlabs.rainforest.data;
 
+import com.detroitlabs.rainforest.model.Product;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
 public class ProductRepository {
+
+    private static final List<String> tag1 = Arrays.asList("tag1");
+    private static final List<String> tag2 = Arrays.asList("tag2");
+    private static final List<String> tag3 = Arrays.asList("tag3");
+
+
+    public static final List<Product> ALL_PRODUCTS = Arrays.asList(
+            new Product(),
+            new Product(),
+            new Product(),
+            new Product(),
+            new Product()
+    );
 }
