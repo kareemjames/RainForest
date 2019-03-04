ca96c311025a4d86b746d6517c3c12c41bf13ac7
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-03-04 11:21:04 -0500

    initial product controller for homepage

--- src/main/java/com/detroitlabs/rainforest/controller/ProductController.java
+++ src/main/java/com/detroitlabs/rainforest/controller/ProductController.java
@@ -1,4 +1,24 @@
 package com.detroitlabs.rainforest.controller;
 
+import com.detroitlabs.rainforest.data.ProductRepository;
+import com.detroitlabs.rainforest.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
 public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @RequestMapping("/")
+    public String displayAllProducts(ModelMap modelMap) {
+        List<Product> allProducts = productRepository.getAllProducts();
+        modelMap.put("allProducts", allProducts);
+        return "home";
+    }
 }
--- src/main/java/com/detroitlabs/rainforest/data/ProductRepository.java
+++ src/main/java/com/detroitlabs/rainforest/data/ProductRepository.java
@@ -1,4 +1,5 @@
 package com.detroitlabs.rainforest.data;
 
 public class ProductRepository {
+
 }
