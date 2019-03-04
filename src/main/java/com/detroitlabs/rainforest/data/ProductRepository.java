f4e96f405098c3505d40e7067799d5e61bec3a45
Mikayla Clemons <mikaylaclemons@Mikaylas-MacBook-Pro.local>
2019-03-04 11:18:54 -0500

    Add arguments into the Product objects in the product repository. Left blank until photos details are added. Create a get all products method in repository.

--- src/main/java/com/detroitlabs/rainforest/data/ProductRepository.java
+++ src/main/java/com/detroitlabs/rainforest/data/ProductRepository.java
@@ -9,16 +9,19 @@ import java.util.List;
 @Component
 public class ProductRepository {
 
-    private static final List<String> tag1 = Arrays.asList("tag1");
-    private static final List<String> tag2 = Arrays.asList("tag2");
-    private static final List<String> tag3 = Arrays.asList("tag3");
+    private static final List<String> photo = Arrays.asList("photo name");
 
 
     public static final List<Product> ALL_PRODUCTS = Arrays.asList(
-            new Product(),
-            new Product(),
-            new Product(),
-            new Product(),
-            new Product()
+            new Product("name ", 0.0, "mini description",1, photo),
+            new Product("name", 0.0, "mini description", 2, photo),
+            new Product("name", 0.0, "mini description", 3, photo),
+            new Product("name", 0.0, "mini description", 4, photo),
+            new Product("name", 0.0, "mini description", 5, photo)
     );
+
+    public List<Product> getAllProducts(){
+        return ALL_PRODUCTS;
+    }
+
 }
