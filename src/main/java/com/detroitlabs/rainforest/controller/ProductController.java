763560fc8e61f4fb1851aeaab789350128cf3bc0
Kareem James <kareemjames@MacBook-Pro-5.local>
2019-03-04 11:53:50 -0500

    add products details page mapping

--- src/main/java/com/detroitlabs/rainforest/controller/ProductController.java
+++ src/main/java/com/detroitlabs/rainforest/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.detroitlabs.rainforest.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -21,4 +22,11 @@ public class ProductController {
         modelMap.put("allProducts", allProducts);
         return "home";
     }
+
+    @RequestMapping("/product/{name}")
+        public String productDetails(@PathVariable String name, ModelMap modelMap) {
+            Product product = productRepository.findByName(name);
+            modelMap.put("product", product);
+            return "product-details";
+        }
 }
